9a01681467d637a58cba41f5aa0716550ddb9283
Mahadev_Balsure <106725711+Mahadev2@users.noreply.github.com>
2022-11-17 05:20:55 -0800

    Create set.cpp

---
+++ set.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    unordered_set <int> s; //you can use unordered_multiset if you want to store any int more than once
+    //set<int>s ==> arrenges values in asending order
+    s.insert(5);
+    s.insert(8);
+    s.insert(3);
+    s.insert(8);
+    
+    //s.begin() ==>Address of the first element of unordered_set
+    //s.end()  ==> Address of last element+one
+    //s.end()-1 ==> Address of last element
+    
+    int a = 2;
+    
+    if(s.find(a) == s.end()){
+        cout<<"Given int is not present"<<endl;
+    }
+    else{
+        cout<<"Yeah I am here"<<endl;
+    }
+}
+    
